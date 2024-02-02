cf54ba4b2e6f202af7808654eec4d6a1fd033193
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-02-02 22:09:27 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -0,0 +1,11 @@
+def solution(phone_number):
+    answer = ''
+    
+    for i in range(len(phone_number)):
+        if i >= len(phone_number) - 4:
+            answer += phone_number[i]
+        else: 
+            answer += '*'
+        
+    
+    return answer
