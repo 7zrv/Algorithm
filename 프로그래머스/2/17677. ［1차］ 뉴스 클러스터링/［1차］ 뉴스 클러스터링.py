3ef08bd39c7327ec751da036dc5c53f84608d37a
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-04-05 03:26:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,58 +1,50 @@
 def solution(str1, str2):
     answer = 0
+    
     str1, str2 = str1.upper(), str2.upper()
+    
+    arr1 = [str1[i:i+2] for i in range(len(str1) - 1) if str1[i:i+2].isalpha()]
+    arr2 = [str2[i:i+2] for i in range(len(str2) - 1) if str2[i:i+2].isalpha()]
+            
+    print(arr1)
+    print(arr2)
+    
     intersection = {}
     union = {}
-    inter = 0
-    uni = 0
-    
-    arr1 = []
-    arr2 = []
     
     dic1 = {}
     dic2 = {}
     
-    
-    
-    for i in range(len(str1)-1):
-        if str1[i].isalpha() and str1[i+1].isalpha():
-            arr1.append(str1[i]+str1[i+1])
-            
-    for i in range(len(str2)-1):
-        if str2[i].isalpha() and str2[i+1].isalpha():
-            arr2.append(str2[i]+str2[i+1])
-            
     for i in arr1:
+        dic1[i] = dic1.get(i, 0) + 1
         
-        if i in dic1:
-            dic1[i] += 1
-        else:
-            dic1[i] = 1
-                
     for i in arr2:
-        if i in dic2:
-            dic2[i] += 1
-        else:
-            dic2[i] = 1
+        dic2[i] = dic2.get(i, 0) + 1
                 
-    union = dic2
+    print(dic1)
+    print(dic2)
                 
     for i in dic1:
         if i in dic2:
             intersection[i] = min(dic1[i], dic2[i])
+            
+    union = dic1.copy()
+    
+    for i in dic2:
+        if i in union:
             union[i] = max(dic1[i], dic2[i])
         else:
-            union[i] = dic1[i] 
+            union[i] = union.get(i, dic2[i])
     
-    for i in intersection:
-        inter += intersection[i] 
+    print(intersection)
+    print(union)
     
-    for i in union:
-        uni += union[i]
+    sum_intersection = sum(intersection.values())
+    sum_union = sum(union.values())
     
-    if inter != 0 and uni != 0:
-        answer = int((inter / uni) * 65536)
-    elif inter == 0 and uni == 0:
+    if sum_intersection != 0 and sum_union != 0:
+        answer = int((sum_intersection / sum_union) * 65536)
+    elif sum_intersection == 0 and sum_union == 0:
         return 65536
     else:
         return 0
