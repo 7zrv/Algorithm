0c2aefdbf35760d1ec3d8e2e40a70c9947abe627
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-04-13 03:08:43 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,15 +1,3 @@
 def solution(targets):
     answer = 0
-    
-    #미사일 범위의 끝나는 부분을 기준으로 정렬
-    targets = sorted(targets, key = lambda x : x[1])
-
-    end = 0
-    
-    for target in targets :
-        #이전 타겟과 겹치는 부분이 없다면 발사
-        if target[0] >= end :
-            end = target[1]
-            answer += 1
-        
     return answer
