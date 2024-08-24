18b280ab1c62f8c45d395ccce70e482503969f97
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-08-24 20:32:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+        in.close();
+        
+        System.out.println(a+b+c);
+        
+    }
+
+}
