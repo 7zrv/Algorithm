21f552c9813e7f25daba82a9d782ea846655c399
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-08-24 18:38:25 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		double A = in.nextInt();
+		double B = in.nextInt();
+        in.close();
+        
+		System.out.println(A/B);
+ 
+		
+	}
+}
