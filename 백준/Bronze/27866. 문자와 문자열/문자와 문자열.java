c57e125951a4644e88fe82bc775405827a5f7bc2
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-08-25 19:22:16 +0900

    [Bronze V] Title: 문자와 문자열, Time: 176 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String str = in.nextLine();
+        int n = in.nextInt();
+
+
+        System.out.println(str.charAt(n-1));
+
+    }
+}
