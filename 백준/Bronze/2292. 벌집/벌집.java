d3575f0d16a26acbdd82c00948843149f1ab9fd0
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-08-30 15:09:44 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int distance = 1;
+        int house = 1;
+
+        while (house < n) {
+            house += 6 * distance++;
+        }
+
+        System.out.println(distance);
+
+    }
+
+}
