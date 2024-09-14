6a769ed29bf09ad4a0ab5789db09badaae50d5f1
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2024-09-14 21:28:00 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int result = 1;
+
+        for(int i = 0; i < 2; i++){
+            result *= Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(result);
+
+    }
+
+}
