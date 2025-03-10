1ed3cf1f7941416edca8c9e244474f34398b662e
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2025-03-10 13:44:59 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.go
@@ -0,0 +1,10 @@
+func solution(num_list []int) []int {
+    
+    arr := make([]int, len(num_list))
+
+    for i := 0; i < len(num_list); i++ {
+        arr[i] = num_list[len(num_list)-1-i]
+    }
+
+    return arr
+}
